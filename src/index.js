45bc67ef518c6ae0b040c783d5b8e1979c42783c
Shawn Su <85855749+WWSDI@users.noreply.github.com>
2022-02-27 12:25:12 +1300

    add: react-redux package; wrap <App> with <Provider>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
 import Application from './Application';
 
 import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Application />
-  </React.StrictMode>,
+  // You have to wrap the entire app with <Provider> to provide redux store to all components via Context API
+  <Provider store={store}>
+    <React.StrictMode>
+      <Application />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
