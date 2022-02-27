4d636aa43212f47c9851db15b8749660670b3c58
Shawn Su <85855749+WWSDI@users.noreply.github.com>
2022-02-27 16:25:43 +1300

    refactor: dispatch callbacks with bindActionCreators()

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, bindActionCreators } from 'react-redux';
 import { increment, decrement, set } from './actions';
 import { SetCounter } from './SetCounter';
 
@@ -6,15 +6,16 @@ export const Counter = () => {
   const incident = 'Incident';
   const count = useSelector((state) => state.count);
   const dispatch = useDispatch();
+  const actions = bindActionCreators({ increment, decrement, set }, dispatch);
 
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
-        <button onClick={() => dispatch(set(0))}>Reset</button>
-        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => actions.decrement()}>Decrement</button>
+        <button onClick={() => actions.set(0)}>Reset</button>
+        <button onClick={() => actions.increment()}>Increment</button>
       </section>
       <SetCounter />
     </main>
