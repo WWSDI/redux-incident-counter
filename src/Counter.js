acd5558dc779464e8ad6d2ce62245ec570355387
Shawn Su <85855749+WWSDI@users.noreply.github.com>
2022-02-27 15:59:01 +1300

    add: dispatch cb to three buttons

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, set } from './actions';
 
 export const Counter = () => {
   const incident = 'Incident';
   const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
 
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(set(0))}>Reset</button>
+        <button onClick={() => dispatch(increment())}>Increment</button>
       </section>
     </main>
   );
