a9848fef9f0da7155efd4776f8832df4e7b33ec5
Shawn Su <85855749+WWSDI@users.noreply.github.com>
2022-02-27 15:59:26 +1300

    add: fix set action error

--- src/reducer.js
+++ src/reducer.js
@@ -5,6 +5,6 @@ export const initialState = { count: 400 };
 export const reducer = (state = initialState, action) => {
   if (action.type === INCREMENT) return { count: state.count + 1 };
   if (action.type === DECREMENT) return { count: state.count - 1 };
-  if (action.type === SET) return { count: state.count + action.payload };
+  if (action.type === SET) return { count: action.payload };
   return state;
 };
