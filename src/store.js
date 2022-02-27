063203a8598f415c0c4ff281537b58efc4ff2979
Shawn Su <85855749+WWSDI@users.noreply.github.com>
2022-02-27 13:03:22 +1300

    add: explanation for redux devtools connection

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
 import { initialState, reducer } from './reducer';
 
+// This is a critical step for making the connection between your app and the redux devtools so that you can use the tools in browser to facilitate development
 const enhancer =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
